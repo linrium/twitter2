90c53193b8e501ababd2b6509019b8a6d68c8e72
linrium <linrium@gmail.com>
2019-10-10 16:57:07 +0700

    update :database

--- config/prod.exs
+++ config/prod.exs
@@ -21,6 +21,7 @@ config :logger, level: :info
 # Configure your database
 config :hello_phoenix, Twitter2.Repo,
   adapter: Ecto.Adapters.Postgres,
+  database: "twitter2_prod",
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
