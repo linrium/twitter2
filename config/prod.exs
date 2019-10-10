9400efe22dff6627d355dbd480a3d6151fd0ce37
linrium <linrium@gmail.com>
2019-10-10 16:51:16 +0700

    fix url

--- config/prod.exs
+++ config/prod.exs
@@ -10,10 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :twitter2, Twitter2Web.Endpoint,
-  url: [scheme: "https", host: "enigmatic-shelf-42426.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "sheltered-brook-01438.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
 
 # Configure your database
 config :hello_phoenix, Twitter2.Repo,
@@ -21,9 +24,6 @@ config :hello_phoenix, Twitter2.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
-
-# Do not print debug messages in production
-config :logger, level: :info
 
 # ## SSL Support
 #
